a6996e7fd27a587a5a05f99840aa6d374ed5933c
Naman Rajput <singhrajputnaman95@gmail.com>
2025-07-11 04:42:55 +0530

    My first Project

---
+++ BabiesInfo.py
@@ -0,0 +1,18 @@
+# CASE : ID Number
+
+# BWT : Birthweight, in ounces (approx 28 grams)
+
+# GESTATION : Length of gestation, in days (the process or period of developing inside the womb between conception and birth)
+
+# PARITY : Binary indicator for the first pregnancy (0 = First pregnancy)
+
+# Age : Mother's age in years
+
+# Height : Mother's height in inches
+
+# Weight : Mother's weight in pounds
+
+# Smoke : Binary indicator for whether the mother smokes 
+
+# pip freeze > requirements.txt
+# pip install pandas numpy matplotlib flask scikit-learn gunicorn
---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, jsonify, render_template
+import pandas as pd 
+import pickle
+
+app = Flask(__name__)
+
+def get_cleaned_data(form_data):
+    gestation = float(form_data["gestation"])
+    parity = int(form_data["parity"])
+    age = float(form_data["age"])
+    height = float(form_data["height"])
+    weight = float(form_data["weight"])
+    smoke = float(form_data["smoke"])
+    
+    cleaned_data = {"gestation" : [gestation],
+                    "parity" : [parity],
+                    "age" : [age],
+                    "height" : [height],
+                    "weight" : [weight],
+                    "smoke" : [smoke]}
+    
+    return cleaned_data
+
+@app.route("/", methods= ["GET"])
+def home():
+    return render_template("index.html")
+
+## Defining the endpoints
+
+@app.route("/predict", methods = ["POST"])
+def get_prediction():
+    
+    # Getting data from user
+    baby_data_form = request.form
+    
+    baby_data_cleaned = get_cleaned_data(baby_data_form)
+
+    # Convert into dataframe
+    baby_df = pd.DataFrame(baby_data_cleaned)
+
+    # Load the machine learning trained model
+    with open("model/model.pkl", "rb") as obj:
+        model = pickle.load(obj)
+        
+    # Make prediction on user data
+    prediction = model.predict(baby_df)
+    prediction = round(float(prediction), 2)
+    
+    # Return the response in a json format
+    response = {"Prediction" : prediction}
+
+    return render_template("index.html", prediction=prediction)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
